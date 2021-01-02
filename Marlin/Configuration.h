66e54f377d92aef33e24e23ccd6368388e807e2b
0x2aff <schlosser.st@gmail.com>
2021-01-02 22:14:01 +0100

    Changes ...

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1840,7 +1840,7 @@
 // @section extras
 
 // Increase the FAN PWM frequency. Removes the PWM noise but increases heating in the FET/Arduino
-#define FAST_PWM_FAN
+//#define FAST_PWM_FAN
 
 // Use software PWM to drive the fan, as for the heaters. This uses a very low frequency
 // which is not as annoying as with the hardware PWM. On the other hand, if this frequency
@@ -1851,7 +1851,7 @@
 // affecting heaters, and the fan if FAN_SOFT_PWM is enabled.
 // However, control resolution will be halved for each increment;
 // at zero value, there are 128 effective control positions.
-//#define SOFT_PWM_SCALE 0
+#define SOFT_PWM_SCALE 0
 
 // If SOFT_PWM_SCALE is set to a value higher than 0, dithering can
 // be used to mitigate the associated resolution loss. If enabled,
